ace7c023e96ee8d69d3d205606773230f502cf9e
Lucas <lucastheodoromourofernandes@gmail.com>
2025-03-28 15:15:41 -0400

    Praticando com Spring: Padrões de Projeto em uma API REST

--- src/main/java/com/theodorol/gof/PadroesProjetosSpringApplication.java
+++ src/main/java/com/theodorol/gof/PadroesProjetosSpringApplication.java
@@ -2,8 +2,10 @@ package com.theodorol.gof;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.theodorol.gof.service")
 public class PadroesProjetosSpringApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/theodorol/gof/service/impl/ClienteServiceImpl.java
+++ src/main/java/com/theodorol/gof/service/impl/ClienteServiceImpl.java
@@ -1,33 +1,64 @@
 package com.theodorol.gof.service.impl;
 
 import com.theodorol.gof.model.Cliente;
+import com.theodorol.gof.model.Endereco;
+import com.theodorol.gof.repository.ClienteRepository;
+import com.theodorol.gof.repository.EnderecoRepository;
 import com.theodorol.gof.service.ClienteService;
+import com.theodorol.gof.service.ViaCepService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
 
 @Service
 public class ClienteServiceImpl implements ClienteService {
+    @Autowired
+    ClienteRepository clienteRepository;
+    @Autowired
+    EnderecoRepository enderecoRepository;
+    @Autowired
+    ViaCepService viaCepService;
     @Override
     public Iterable<Cliente> buscarTodos() {
-        return null;
+        return clienteRepository.findAll();
     }
 
     @Override
     public Cliente buscarPorId(Long id) {
-        return null;
+        return clienteRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("cliente invalido"));
     }
 
     @Override
     public void inserirCliente(Cliente cliente) {
-
+        salvarClienteCep(cliente);
     }
 
     @Override
     public void atualizarCliente(Long id, Cliente cliente) {
-
+        Optional<Cliente> clienteDb = clienteRepository.findById(id);
+        if(clienteDb.isEmpty()){
+            throw new RuntimeException("cliente invalido");
+        }
+        salvarClienteCep(cliente);
     }
 
     @Override
     public void deletarCliente(Long id) {
+        clienteRepository.deleteById(id);
+    }
 
+    private void salvarClienteCep(Cliente cliente){
+        String cep = cliente.getEndereco().getCep();
+        Endereco endereco = enderecoRepository.findById(cep)
+                .orElseGet(()->{
+                    Endereco novoEndereco = viaCepService.consultarCep(cep);
+                    enderecoRepository.save(novoEndereco);
+                    return novoEndereco;
+                });
+        cliente.setEndereco(endereco);
+        clienteRepository.save(cliente);
     }
 }
